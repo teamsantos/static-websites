e4e0d9eec34f385ff487f386c7ff19a5997dd420
Filipe Santos <filipesantosdev@gmail.com>
2025-08-25 23:38:33 +0100

    WIP

--- infra/bucketStack.js
+++ infra/bucketStack.js
@@ -40,37 +40,51 @@ const s3 = __importStar(require("aws-cdk-lib/aws-s3"));
 class BucketStack extends cdk.Stack {
     constructor(scope, id, props) {
         super(scope, id, props);
-        // Create the S3 bucket for static websites
-        this.bucket = new s3.Bucket(this, "StaticWebsitesBucket", {
-            bucketName: props.bucketName,
-            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-            encryption: s3.BucketEncryption.S3_MANAGED,
-            versioned: false,
-            removalPolicy: cdk.RemovalPolicy.RETAIN,
-        });
+        // Check if the bucket already exists and reference it instead of creating
+        let bucket;
+        try {
+            // Try to reference the existing bucket
+            bucket = s3.Bucket.fromBucketName(this, "ExistingStaticWebsitesBucket", props.bucketName);
+            console.log(`Using existing S3 bucket: ${props.bucketName}`);
+        }
+        catch (error) {
+            // If the bucket doesn't exist, create it
+            bucket = new s3.Bucket(this, "StaticWebsitesBucket", {
+                bucketName: props.bucketName,
+                blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+                encryption: s3.BucketEncryption.S3_MANAGED,
+                versioned: false,
+                removalPolicy: cdk.RemovalPolicy.RETAIN,
+            });
+            console.log(`Created new S3 bucket: ${props.bucketName}`);
+        }
+        this.bucket = bucket;
         // Add bucket policy that allows any CloudFront distribution in this account to get objects
-        new s3.BucketPolicy(this, "CloudFrontAccessPolicy", {
-            bucket: this.bucket,
-        }).document.addStatements(new iam.PolicyStatement({
-            sid: "AllowCloudFrontServicePrincipalForAccount",
-            effect: iam.Effect.ALLOW,
-            principals: [new iam.ServicePrincipal("cloudfront.amazonaws.com")],
-            actions: ["s3:GetObject"],
-            resources: [`${this.bucket.bucketArn}/*`],
-            conditions: {
-                StringEquals: {
-                    "AWS:SourceAccount": cdk.Stack.of(this).account,
+        // Note: This will only work if we created the bucket, not if we're referencing an existing one
+        if (bucket instanceof s3.Bucket) {
+            new s3.BucketPolicy(this, "CloudFrontAccessPolicy", {
+                bucket: bucket,
+            }).document.addStatements(new iam.PolicyStatement({
+                sid: "AllowCloudFrontServicePrincipalForAccount",
+                effect: iam.Effect.ALLOW,
+                principals: [new iam.ServicePrincipal("cloudfront.amazonaws.com")],
+                actions: ["s3:GetObject"],
+                resources: [`${bucket.bucketArn}/*`],
+                conditions: {
+                    StringEquals: {
+                        "AWS:SourceAccount": cdk.Stack.of(this).account,
+                    },
                 },
-            },
-        }));
+            }));
+        }
         // Output the bucket name for reference
         new cdk.CfnOutput(this, "BucketName", {
-            value: this.bucket.bucketName,
+            value: bucket.bucketName,
             description: "Name of the S3 bucket for static websites",
             exportName: `${this.stackName}-BucketName`,
         });
         new cdk.CfnOutput(this, "BucketArn", {
-            value: this.bucket.bucketArn,
+            value: bucket.bucketArn,
             description: "ARN of the S3 bucket for static websites",
             exportName: `${this.stackName}-BucketArn`,
         });
