e88f6d7389e982ce11b9eae7f73add9b2e6d9ad3
Filipe Santos <filipesantosdev@gmail.com>
2026-01-03 01:04:16 +0000

    dev

--- infra/bucketStack.js
+++ infra/bucketStack.js
@@ -59,13 +59,30 @@ class BucketStack extends cdk.Stack {
             console.log(`Created new S3 bucket: ${props.bucketName}`);
         }
         this.bucket = bucket;
-        // Add bucket policy that allows any CloudFront distribution in this account to get objects
-        // Note: This will only work if we created the bucket, not if we're referencing an existing one
+        // Add bucket policy that allows CloudFront OAC to access objects
+        // This policy allows any CloudFront OAC principal in the same account to access the bucket
         if (bucket instanceof s3.Bucket) {
-            new s3.BucketPolicy(this, "CloudFrontAccessPolicy", {
+            new s3.BucketPolicy(this, "CloudFrontOACPolicy", {
                 bucket: bucket,
             }).document.addStatements(new iam.PolicyStatement({
-                sid: "AllowCloudFrontServicePrincipalForAccount",
+                sid: "AllowCloudFrontOACAccess",
+                effect: iam.Effect.ALLOW,
+                principals: [new iam.ServicePrincipal("cloudfront.amazonaws.com")],
+                actions: ["s3:GetObject"],
+                resources: [`${bucket.bucketArn}/*`],
+                conditions: {
+                    StringEquals: {
+                        "AWS:SourceAccount": cdk.Stack.of(this).account,
+                    },
+                },
+            }));
+        }
+        else {
+            // For imported buckets, create a bucket policy construct
+            new s3.BucketPolicy(this, "CloudFrontOACPolicyImported", {
+                bucket: bucket,
+            }).document.addStatements(new iam.PolicyStatement({
+                sid: "AllowCloudFrontOACAccessImported",
                 effect: iam.Effect.ALLOW,
                 principals: [new iam.ServicePrincipal("cloudfront.amazonaws.com")],
                 actions: ["s3:GetObject"],
