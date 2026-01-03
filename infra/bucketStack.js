eda86fc337f71927ad110203fa903b2aab708522
Filipe Santos <filipesantosdev@gmail.com>
2026-01-03 01:10:35 +0000

    dev

--- infra/bucketStack.js
+++ infra/bucketStack.js
@@ -42,10 +42,12 @@ class BucketStack extends cdk.Stack {
         super(scope, id, props);
         // Check if the bucket already exists and reference it instead of creating
         let bucket;
+        let bucketCreated = false;
         try {
             // Try to reference the existing bucket
             bucket = s3.Bucket.fromBucketName(this, "ExistingStaticWebsitesBucket", props.bucketName);
             console.log(`Using existing S3 bucket: ${props.bucketName}`);
+            bucketCreated = false;
         }
         catch (error) {
             // If the bucket doesn't exist, create it
@@ -57,32 +59,16 @@ class BucketStack extends cdk.Stack {
                 removalPolicy: cdk.RemovalPolicy.RETAIN,
             });
             console.log(`Created new S3 bucket: ${props.bucketName}`);
+            bucketCreated = true;
         }
         this.bucket = bucket;
-        // Add bucket policy that allows CloudFront OAC to access objects
-        // This policy allows any CloudFront OAC principal in the same account to access the bucket
-        if (bucket instanceof s3.Bucket) {
+        // Only manage bucket policy if we created the bucket
+        // For imported buckets, assume the policy is already configured or managed externally
+        if (bucketCreated && bucket instanceof s3.Bucket) {
             new s3.BucketPolicy(this, "CloudFrontOACPolicy", {
                 bucket: bucket,
             }).document.addStatements(new iam.PolicyStatement({
                 sid: "AllowCloudFrontOACAccess",
-                effect: iam.Effect.ALLOW,
-                principals: [new iam.ServicePrincipal("cloudfront.amazonaws.com")],
-                actions: ["s3:GetObject"],
-                resources: [`${bucket.bucketArn}/*`],
-                conditions: {
-                    StringEquals: {
-                        "AWS:SourceAccount": cdk.Stack.of(this).account,
-                    },
-                },
-            }));
-        }
-        else {
-            // For imported buckets, create a bucket policy construct
-            new s3.BucketPolicy(this, "CloudFrontOACPolicyImported", {
-                bucket: bucket,
-            }).document.addStatements(new iam.PolicyStatement({
-                sid: "AllowCloudFrontOACAccessImported",
                 effect: iam.Effect.ALLOW,
                 principals: [new iam.ServicePrincipal("cloudfront.amazonaws.com")],
                 actions: ["s3:GetObject"],
