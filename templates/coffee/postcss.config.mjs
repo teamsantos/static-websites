59858acc6620bf216078bec6e7fb44e9a799f488
Filipe Santos <filipesantosdev@gmail.com>
2026-01-11 18:00:12 +0000

    Cleanedup coffee template

--- templates/coffee/postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    // PostCSS plugins are not needed when using Tailwind CSS CDN
-  },
-}
-
-export default config
